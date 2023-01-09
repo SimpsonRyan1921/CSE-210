b4a6ddbc2c694b9ce3556d74377456f6569fa11b
Ryan Simpson <simpson939@byui.edu>
2023-01-09 11:41:27 -0700

    Finished Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage in the class? ");
+        string grade = Console.ReadLine();
+        int x = int.Parse(grade);
+
+
+        if (x > 90)
+        {
+            Console.Write("You have an A in the class.");
+        }
+        
+        else if (x > 80)
+        {
+            Console.Write("You have a B in the class.");
+        }
+        else if (x > 70)
+        {
+            Console.Write("You have a C in the class.");
+        }
+        else if (x > 60)
+        {
+            Console.Write("You have a D in the class.");
+        }
+        else
+        {
+            Console.Write("You have failed the class.");
+        }
     }
 }
