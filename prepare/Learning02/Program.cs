e669466816da6f4e1b2f259274ea5408229918c5
Ryan Simpson <simpson939@byui.edu>
2023-01-18 18:35:23 -0700

    Finished Abstract Learning Activit Week 3

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Lifeguard";
+        job1._company = "Blueray";
+        job1._startYear = 2015 ;
+        job1._endYear = 2017 ;
+
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Audio/Video/Lighting Utility";
+        job2._company = "Showpro";
+        job2._startYear = 2018 ;
+        job2._endYear = 2023 ;
+
+        Resmue myResume = new Resmue();
+        myResume._name = "Ryan Simpson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+       
     }
 }
